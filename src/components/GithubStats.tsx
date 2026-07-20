be857c4829d964d7fae87bf3fceaefdd4b139d86
Muhammad Rifki <60310528+ikiytech@users.noreply.github.com>
2026-07-20 21:28:15 +0700

    Update GithubStats.tsx

--- src/components/GithubStats.tsx
+++ src/components/GithubStats.tsx
@@ -17,14 +17,14 @@ export default function GithubStats() {
           <div className="grid md:grid-cols-2 gap-4">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              src={`https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&theme=dark&hide_border=true&bg_color=1c1c1a&title_color=f2a900&text_color=e8e8e4&icon_color=f2a900`}
+              src="https://github-readme-stats-eight-theta.vercel.app/api?username=ikiytech&show_icons=true&theme=dark&hide_border=true&bg_color=1c1c1a&title_color=f2a900&text_color=e8e8e4&icon_color=f2a900"
               alt={`GitHub stats ${githubUsername}`}
               loading="lazy"
               className="max-w-full h-auto rounded-lg mx-auto"
             />
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              src={`https://github-readme-streak-stats.herokuapp.com/?user=${githubUsername}&theme=dark&hide_border=true&background=1c1c1a&ring=f2a900&fire=f2a900&currStreakLabel=e8e8e4`}
+              src="https://github-readme-streak-stats.herokuapp.com/?user=ikiytech&theme=dark&hide_border=true&background=1c1c1a&ring=f2a900&fire=f2a900&currStreakLabel=e8e8e4"
               alt={`GitHub streak ${githubUsername}`}
               loading="lazy"
               className="max-w-full h-auto rounded-lg mx-auto"
